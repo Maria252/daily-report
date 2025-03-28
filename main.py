139c089f74c4f7688ab9119bba3f59a3c2dced26
MariaDiaz <90877410+Maria252@users.noreply.github.com>
2025-03-28 07:43:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.oauth2 import service_account
 import requests
 from google.auth.transport.requests import Request
 import json
